47145c2328322b42641149a51edc2238fd338064
0ct4vian <octavianpaduroiu1@gmail.com>
2023-10-30 15:55:56 +0200

    commit #7 return view mustache

--- public/index.php
+++ public/index.php
@@ -3,37 +3,23 @@
 use App\Http\Controllers\HomeController;
 use Symfony\Component\HttpFoundation\Request;
 
-require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ .  "/../routes/web.php";
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ .  "/../routes/web.php";
 
 //$framework->init();
 $builder = new \DI\ContainerBuilder();
 $container = $builder->build();
 
 $container->set('request', Request::createFromGlobals());
-//$container->set(\Bootstrap\Contracts\Storage\Storage::class, new class implements \Bootstrap\Contracts\Storage\Storage {
-//    public function get(): string
-//    {
-//        return 'demo';
-//    }
-//});
-$controller= $container->get(HomeController::class);
-//$container->call([$controller, 'showDemo']);
-
-
-
-$m = new Mustache_Engine(array(
-    'loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__).'\\..\\resources\\views'),
-));
-
+$container->set('controller', new HomeController($container->get('request')));
+$container->set(('mustacheEngine'), new Mustache_Engine(array(
+    'loader' => new Mustache_Loader_FilesystemLoader(getcwd(). '\\..\\resources\\views\\')
+)));
 //$template = $m->loadTemplate("\\pages\\home.mustache");
-//echo $m->render("\\pages\\home.mustache", ['user' => \Symfony\Component\Uid\Ulid::generate(), 'name' => 'user']);
+//echo $m->render("\\pages\\home.mustache", );
 
 $router = new \Bootstrap\Routing\Router($container);
-
-$router->get('demo', 'HomeController@showDemo');
-
+$router->setNamespace('App\\Http\\Controllers\\');
+$router->get('demo', $container->call('controller::showDemo'));
 $router->run();
-
-
 
--- src/App/Http/Controllers/HomeController.php
+++ src/App/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Bootstrap\Contracts\Storage\Storage;
+use DI\Container;
+use Mustache_Engine;
 use Symfony\Component\HttpFoundation\Request;
 
 class HomeController extends Controller
@@ -11,11 +13,13 @@ class HomeController extends Controller
 
     }
 
-    public function showDemo()
+    public function showDemo(Container $container)
     {
 //        return $this->storage->get();
-        echo 'demo';
+        echo $container->call('mustacheEngine::render', ['\\pages\\home.mustache',  ['user' => \Symfony\Component\Uid\Ulid::generate(), 'name' => 'user']]);
+//        $tpl = $m->loadTemplate('pages\\home.mustache');
     }
+
 
 
 }
